3e4827160811cb547ce4073653de9a0523319004
Surface <Surface@DESKTOP-UGVIP3L>
2023-03-08 14:49:50 +0200

    Add project files.

---
+++ net.6 WebApp1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace net._6_WebApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
