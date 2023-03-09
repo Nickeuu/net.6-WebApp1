c994eb3999e45d0a947888d2991bccafda41908a
Surface <Surface@DESKTOP-UGVIP3L>
2023-03-09 09:04:05 +0200

    Save Commit

---
+++ net.6 WebApp1/Data/Enum/ClubCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace net._6_WebApp1.Data.Enum
+{
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
---
+++ net.6 WebApp1/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace net._6_WebApp1.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+    }
+}
