2ec12cff0a1013d8782072a1ee9503154084e73d
Surface <Surface@DESKTOP-UGVIP3L>
2023-03-09 09:34:57 +0200

    Commit Save

--- net.6 WebApp1/Data/ApplicationDbContext.cs
+++ net.6 WebApp1/Data/ApplicationDbContext.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using net._6_WebApp1.Models;
 
 namespace net._6_WebApp1.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
     }
 }
