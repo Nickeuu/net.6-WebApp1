e4635ef4b2785cfcb82cb626530394f65a20c917
Surface <Surface@DESKTOP-UGVIP3L>
2023-03-11 13:27:07 +0200

    save

--- net.6 WebApp1/Controllers/ClubController.cs
+++ net.6 WebApp1/Controllers/ClubController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using net._6_WebApp1.Data;
+using net._6_WebApp1.Models;
 
 namespace net._6_WebApp1.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
