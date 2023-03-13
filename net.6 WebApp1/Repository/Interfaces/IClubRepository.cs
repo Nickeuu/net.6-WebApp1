08ebef7af704aa0b349f85edc38a4a7aa4c94682
Surface <Surface@DESKTOP-UGVIP3L>
2023-03-13 11:52:21 +0200

    save

---
+++ net.6 WebApp1/Repository/Interfaces/IClubRepository.cs
@@ -0,0 +1,17 @@
+﻿using net._6_WebApp1.Models;
+
+namespace net._6_WebApp1.Repository.Interfaces
+{
+    public interface IClubRepository
+    {
+        Task<IEnumerable<Club>> GetAll();
+        Task<Club> GetByIdAsync(int id);
+        Task<IEnumerable<Club>> GetClubByCity(string city);
+        bool Add(Club club);
+        bool Update(Club club);
+        bool Delete(Club club);
+        bool Save();
+
+
+    }
+}
