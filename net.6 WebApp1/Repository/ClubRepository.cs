5e1f778c650c575d5b36f4d121d6a9fd27fcd672
Surface <Surface@DESKTOP-UGVIP3L>
2023-03-13 15:16:57 +0200

    save

--- net.6 WebApp1/Repository/ClubRepository.cs
+++ net.6 WebApp1/Repository/ClubRepository.cs
@@ -33,7 +33,7 @@ namespace net._6_WebApp1.Repository
 
         public async Task<Club> GetByIdAsync(int id)
         {
-            return await _context.Clubs.FirstOrDefaultAsync(i => i.Id == id);
+            return await _context.Clubs.Include(i => i.Address).FirstOrDefaultAsync(i => i.Id == id);
         }
 
         public async Task<IEnumerable<Club>> GetClubByCity(string city)
