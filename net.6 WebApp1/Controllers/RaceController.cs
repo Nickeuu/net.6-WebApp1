36354c8b6eaceeb782400ed8b92d2b6a73e21cc0
Surface <Surface@DESKTOP-UGVIP3L>
2023-03-14 20:13:18 +0200

    save

--- net.6 WebApp1/Controllers/RaceController.cs
+++ net.6 WebApp1/Controllers/RaceController.cs
@@ -2,40 +2,65 @@
 using net._6_WebApp1.Interfaces;
 using net._6_WebApp1.Models;
 using net._6_WebApp1.Repository;
+using net._6_WebApp1.Services;
+using net._6_WebApp1.ViewModels;
 
 namespace net._6_WebApp1.Controllers
 {
     public class RaceController : Controller
     {
-        private readonly IRaceRepository _raceRepository;
+        private readonly IRaceRepository _raceRespository;
+        private readonly IPhotoService _photoService;
 
-        public RaceController(IRaceRepository raceRepository)
+        public RaceController(IRaceRepository raceRespository, IPhotoService photoService)
         {
-            _raceRepository = raceRepository;
+            _raceRespository = raceRespository;
+            _photoService = photoService;
         }
         public async Task<IActionResult> Index()
         {
-            IEnumerable<Race> races = await _raceRepository.GetAll();
+            IEnumerable<Race> races = await _raceRespository.GetAll();
             return View(races);
         }
         public async Task<IActionResult> Detail(int id)
         {
-            Race race = await _raceRepository.GetByIdAsync(id);
+            Race race = await _raceRespository.GetByIdAsync(id);
             return View(race);
         }
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
-        public async Task<IActionResult> Create(Race race)
+        public async Task<IActionResult> Create(CreateRaceViewModel raceVM)
         {
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                return View(race);
+                var result = await _photoService.AddPhotoAsync(raceVM.Image);
+
+                var race = new Race
+                {
+                    Title = raceVM.Title,
+                    Description = raceVM.Description,
+                    Image = result.Url.ToString(),
+                    Address = new Address
+                    {
+                        Street = raceVM.Address.Street,
+                        City = raceVM.Address.City,
+                        State = raceVM.Address.State,
+                    }
+                };
+                _raceRespository.Add(race);
+                return RedirectToAction("Index");
             }
-            _raceRepository.Add(race);
-            return RedirectToAction("Index");
+            else
+            {
+                ModelState.AddModelError("", "Photo upload failed");
+            }
+
+            return View(raceVM);
         }
     }
 }
+
---
+++ net.6 WebApp1/ViewModels/CreateRaceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using net._6_WebApp1.Data.Enum;
+using net._6_WebApp1.Models;
+
+namespace net._6_WebApp1.ViewModels
+{
+    public class CreateRaceViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Address Address { get; set; }
+        public IFormFile Image { get; set; }
+        public RaceCategory RaceCategory { get; set; }
+    }
+}
