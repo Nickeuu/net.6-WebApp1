2163360550a93ec31a6c53bc41a81d8618ad7829
Surface <Surface@DESKTOP-UGVIP3L>
2023-03-18 18:03:37 +0200

    Working register and login

--- net.6 WebApp1/Controllers/AccountController.cs
+++ net.6 WebApp1/Controllers/AccountController.cs
@@ -55,13 +55,14 @@ namespace net._6_WebApp1.Controllers
             var response = new RegisterViewModel();
             return View(response);
         }
+
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
         {
             if (!ModelState.IsValid) return View(registerViewModel);
 
             var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
-            if(user != null)
+            if (user != null)
             {
                 TempData["Error"] = "This email address is already in use";
                 return View(registerViewModel);
@@ -70,13 +71,14 @@ namespace net._6_WebApp1.Controllers
             var newUser = new AppUser()
             {
                 Email = registerViewModel.EmailAddress,
-                UserName = registerViewModel.EmailAddress,
+                UserName = registerViewModel.EmailAddress
             };
             var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+
             if (newUserResponse.Succeeded)
                 await _userManager.AddToRoleAsync(newUser, UserRoles.User);
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Race");
         }
 
         [HttpPost]
@@ -85,5 +87,7 @@ namespace net._6_WebApp1.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Race");
         }
+
+
     }
 }
