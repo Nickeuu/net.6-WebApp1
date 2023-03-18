149d16aa9c0ab4c76fe0d031a06dd8bda2c42b28
Surface <Surface@DESKTOP-UGVIP3L>
2023-03-18 15:02:39 +0200

    Database migration address id nullable and working register

--- net.6 WebApp1/Controllers/AccountController.cs
+++ net.6 WebApp1/Controllers/AccountController.cs
@@ -43,12 +43,47 @@ namespace net._6_WebApp1.Controllers
                     }
                 }
                 //Password is incorrect
-                TempData["Error"] = "Wrong credidentials. Try agane!";
+                TempData["Error"] = "Wrong credidentials. Try again!";
                 return View(loginViewModel);
             }
             //User not found
             TempData["Error"] = "Wrong credidentials. Please try again!";
             return View(loginViewModel);
+        }
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+
+            var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+            if(user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerViewModel);
+            }
+
+            var newUser = new AppUser()
+            {
+                Email = registerViewModel.EmailAddress,
+                UserName = registerViewModel.EmailAddress,
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Race");
         }
     }
 }
